0efce835e0118730dd3b2dca911533f058643f88
Coby McKinney <31231058+oyciny@users.noreply.github.com>
2021-08-14 00:37:55 -0500

    Added Files

---
+++ server-setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# This script aids in the initial server setup process.
+# To use it simply download it to your root directory
+# upon logging in as root for the first time and run the
+# following command to make it executable:
+#
+# > chmod +x server-setup.sh
+#
+# Then run the following command to run the setup and you're
+# done! Hope you enjoy.
+
+ORANGE="\033[0;33m"
+RED="\033[0;31m"
+
+function isRoot() {
+    if [ "${EUID}" -ne 0 ]; then
+        echo "${RED}You need to run this script as root!${NC}"
+        exit 1
+    fi
+}
+
+function initialCheck() {
+    isRoot
+}
+
+function installQuestions() {
+    echo "${ORANGE}Welcome to the Initial Server Setup Script!${NC}"
+    echo ""
+    echo "I need to ask you a few questions before starting the setup."
+    echo "If you are okay with the default options just hit enter."
+
+    # Get new user profile name
+    until [[ ${SERVER_USER_NAME} =~ ^[a-zA-Z0-9.]+$ ]]; do
+        read -rp "${ORANGE}What should we call your new user:${NC} " -e -i "sammy" SERVER_USER_NAME
+    done
+
+    # Get new user password
+    until [[ ${USER_PASSWORD} =~ ^[a-zA-Z0-9.]+$ ]]; do
+        read -rp "${ORANGE}What should we set the password for${NC} ${SERVER_USER_NAME} ${ORANGE}to:${NC} " -e -i USER_PASSWORD
+    done
+
+    # SSH Keys?
+    read -rp "${ORANGE} Are you currently using a SSH Key to log into this server?${NC} [Y/n]: " -e -i -n SSH_KEYS
+    if [[ $SSH_KEYS == 'Y' || $SSH_KEYS == 'y' ]]; then
+        SSH_KEY_OPTION=true
+    else
+        SSH_KEY_OPTION=false
+    fi
+
+    echo "Okay that is all I needed to know! We are ready to do the initial setup of your server."
+    echo "Once I have everything done you will be logged into your new user."
+    read -n1 -r -p "Press any key to continue..."
+}
+
+function serverSetup() {
+    # Gather details from user
+    installQuestions
+}
+
+initialCheck
+serverSetup
